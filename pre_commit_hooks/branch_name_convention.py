e8e19e6febe594514eaf5d4e5c6b3294b10ae23e
obarbier <obarbier@obarbier.obarbier>
2020-07-15 20:19:19 -0400

    branch_name-convention

--- pre_commit_hooks/branch_name_convention.py
+++ pre_commit_hooks/branch_name_convention.py
@@ -1,13 +1,31 @@
-import argparse
-def main(argv: Optional[Sequence[str]] = None) -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'filenames', nargs='*',
-        help='Filenames pre-commit believes are changed.',
-    )
 
+import re 
+import argparse
+from typing import Optional
+from typing import Sequence
+from typing import Set
+
+from pre_commit_hooks.util import CalledProcessError
+from pre_commit_hooks.util import cmd_output
+
+
+def current_branch_name() -> Set[str]:
+    try:
+        # git branch --show-current
+        branch = cmd_output('git', 'branch', '--show-current')
+    except CalledProcessError:  # pragma: no cover (with git-lfs)
+        branch = 'master'
+    return branch
+
+
+def main(argv: Optional[str] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pattern', help='Pattern to check')
     args = parser.parse_args(argv)
-    print(args)
+    cBranch = current_branch_name()
+    if re.match(args.pattern, cBranch) is not None:
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
